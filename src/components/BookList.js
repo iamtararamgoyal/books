1650d84326f127ff62b579ce2d29b1cbd0c0c508
TararamGoyal <tara.goyal@programmers.io>
2023-12-13 15:24:55 +0530

    context implementing

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,18 @@
+import { useContext } from 'react';
+import BooksContext from '../context/book';
 import BookShow from './BookShow'
 
 function BookList({ books, onDelete, onEdit }) {
 
+    const { count, incrementCount } = useContext(BooksContext);
     const renderedBooks = books.map((book) => {
         return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />
     });
 
-    return <div className='book-list'>{renderedBooks}</div>
+    return <div className='book-list'>
+        <h1>{count}</h1>
+        <button onClick={incrementCount}>click</button>
+        {renderedBooks}</div>
 }
 
 export default BookList
